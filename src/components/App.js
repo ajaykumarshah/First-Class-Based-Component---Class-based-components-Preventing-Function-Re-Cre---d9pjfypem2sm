55e9c55238dc5b4a87d9d72be047dc09f78a7ee8
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-18 02:18:29 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,17 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-  
+    state = { name: "John Doe", enrollmentNo: "12345678", age: 34 };
+
+    render() {
+        const handleclick = () => {
+            this.setState({ name: this.state.name, enrollmentNo: this.state.enrollmentNo, age: this.state.age + 1 });
+        }
+        return <div>
+            Hello, my name is <h1>{this.state.name}</h1> and 'I am <p>{this.state.age}</p> years old and my enrollment no is <p>{this.state.enrollmentNo} </p>
+            <button onClick={handleclick} >inrease age</button>
+        </div>
+    }
 }
 
 
