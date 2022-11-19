d6a30b78c6c299613945a5cab7b684c81729b052
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-19 15:15:15 +0530

    k

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-
-    state = { name: "John Doe", enrollmentNo: "12345678", age: 34 };
-    handleclick = () => {
+    handleclick() {
 
         this.setState({ age: this.state.age + 1 });
 
     }
+    constructor(props) {
+        super(props);
+        this.state = { name: "John Doe", enrollmentNo: "12345678", age: 34 };
+        this.handleclick = this.handleclick.bind(this);
+    }
+
+
+
     render() {
 
         return <div>
