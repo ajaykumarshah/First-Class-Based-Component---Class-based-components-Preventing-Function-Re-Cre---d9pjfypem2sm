b3940e102efadcdf477651881570e09df4f14523
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-30 15:49:47 +0530

    h

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,55 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
-    handleclick() {
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+
+            name: "John Doe",
+
+            enrollmentNo: "12345678",
+
+            age: 34,
+
+        };
+
+    }
+
+    changeAge = () => {
 
         this.setState({ age: this.state.age + 1 });
 
     }
-    constructor(props) {
-        super(props);
-        this.state = { name: "John Doe", enrollmentNo: "12345678", age: 34 };
-        this.handleclick = this.handleclick.bind(this);
-    }
-
-
 
     render() {
 
-        return <div>
-            <h1>Hello, my name is {this.state.name}</h1>
-            <p>'I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}</p>
-            <button onClick={this.handleclick} >inrease age</button>
-        </div>
+        return (
+
+            <div>
+
+                <h1>Hello, my name is {this.state.name}</h1>
+
+                <p>
+
+                    I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}
+
+                </p>
+
+                <button
+
+                    type="button"
+
+                    onClick={this.changeAge}
+
+                >Increase Age</button>
+
+            </div>
+
+        );
+
     }
 }
-
 
 export default App;
